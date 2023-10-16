cc30a1f27493e7b8260a03e712f3cd087216b593
Ravilochan <ravilochan2000@gmail.com>
2023-10-16 17:51:13 +0530

    update traefik service name

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.use(express.json())
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: false }));
 app.use(cors())
+app.use(helmet())
 app.use(morgan('dev'))
 
 const { dogsData, catsData } = require("./data")
